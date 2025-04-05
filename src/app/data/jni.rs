62f79012084c690505ec1c0ad85387fa240d094b
C0j23T <1697156270@qq.com>
2025-04-05 18:09:48 +0800

    Impl jni data switching. The project is finished

--- src/app/data/jni.rs
+++ src/app/data/jni.rs
@@ -1,3 +1,5 @@
+#![allow(dead_code)]
+
 use std::{
     sync::mpsc::{Receiver, Sender},
     time::{Duration, Instant},
@@ -12,7 +14,7 @@ use crate::app::structs::{Player, PlayerRecord};
 
 use super::{DataManager, TryRecvError};
 
-#[derive(Clone, PartialEq, Eq)]
+#[derive(PartialEq, Eq)]
 pub enum RequestBody {
     GetCurrentPlayer,
     GetPlayersBestExceptSelf,
@@ -23,6 +25,7 @@ pub enum RequestBody {
     FindPlayer(Player),
     UpdatePlayer(Player),
     RemovePlayer(Player),
+    Exit,
 }
 
 impl RequestBody {
@@ -37,11 +40,12 @@ impl RequestBody {
             Self::FindPlayer { .. } => 6,
             Self::UpdatePlayer { .. } => 7,
             Self::RemovePlayer { .. } => 8,
+            Self::Exit => 9,
         }
     }
 }
 
-#[derive(Clone, PartialEq, Eq)]
+#[derive(PartialEq, Eq)]
 pub enum ResponseBody {
     GetCurrentPlayer(Player),
     GetPlayersBestExceptSelf(Vec<Player>),
@@ -105,6 +109,12 @@ impl JniDataManager {
         self.requests.retain(|x| !expired.contains(&x.0.1));
         self.responses.retain(|x| !expired.contains(&x.1));
     }
+
+    fn update_responses(&mut self) {
+        while let Ok(resp) = self.rx.try_recv() {
+            self.responses.push(resp);
+        }
+    }
 }
 
 macro_rules! impl_request_response {
@@ -118,6 +128,9 @@ macro_rules! impl_request_response {
     };
     ($this:ident, $request:ident, $rsp_variant:ident,) => {
         // This comma 🤯
+        $this.update_responses();
+        $this.clear_expired();
+
         let request_ty = $request.to_ty();
         let has_pending = $this.requests.iter().any(|((r, _), _)| *r == request_ty);
 
@@ -140,6 +153,7 @@ macro_rules! impl_request_response {
         if let Some((index, seq)) = r {
             if $this.requests.iter().any(|((_, id), _)| *id == seq) {
                 let (resp, _) = $this.responses.remove(index);
+                $this.requests.retain(|x| x.0.1 != seq);
                 let ResponseBody::$rsp_variant(result) = resp else {
                     unreachable!()
                 };
@@ -150,7 +164,6 @@ macro_rules! impl_request_response {
         if $this.check_expired(request_ty) {
             return Err(TryRecvError::Timeout);
         }
-        $this.clear_expired();
 
         return Err(TryRecvError::Empty);
     }
@@ -206,66 +219,75 @@ impl DataManager for JniDataManager {
     }
 }
 
+fn parse_java_list<'local, T, F>(
+    env: &mut JNIEnv<'local>,
+    list: &JObject<'local>,
+    parser: F,
+) -> jni::errors::Result<Vec<T>>
+where
+    F: Fn(&mut JNIEnv<'local>, &JObject<'local>) -> jni::errors::Result<T>,
+{
+    if list.is_null() {
+        return Ok(Vec::default());
+    }
+    let size = env.call_method(&list, "size", "()I", &[])?.i()?;
+    let mut result = Vec::new();
+    for i in 0..size {
+        let obj = env.call_method(
+            &list,
+            "get",
+            "(I)Ljava/lang/Object;",
+            &[jni::objects::JValueGen::Int(i)],
+        )?;
+        result.push(parser(env, &obj.try_into()?)?);
+    }
+    Ok(result)
+}
+
 fn get_player_from_java<'local>(
     env: &mut JNIEnv<'local>,
-    o: JObject<'local>,
+    o: &JObject<'local>,
 ) -> jni::errors::Result<Option<Player>> {
-    let clazz = env
-        .find_class("com/smoother/TacticalGrid2048/entity/Player")
-        .unwrap();
-    let flag = env
-        .is_instance_of(env.get_object_class(&o).unwrap(), clazz)
-        .unwrap();
+    if o.is_null() {
+        return Ok(None);
+    }
+    let clazz = env.find_class("com/smoother/TacticalGrid2048/entity/Player")?;
+    let flag = env.is_instance_of(o, clazz)?;
     if !flag {
         return Ok(None);
     }
 
-    let id = env.get_field(&o, "id", "I")?.i()?;
-    let name = env.get_field(&o, "name", "Ljava/lang/String;")?;
-    let best_score = env.get_field(&o, "score", "I")?.i()?;
-    let best_time = env.get_field(&o, "time", "J")?.j()?;
-    let best_timestamp = env.get_field(&o, "timestamp", "J")?.j()?;
-    let name: String = if let JValueGen::Object(o) = name {
+    let id = env.get_field(o, "id", "I")?.i()?;
+    let name = {
+        let o: JObject<'_> = env.get_field(o, "name", "Ljava/lang/String;")?.try_into()?;
         if o.is_null() {
             String::new()
         } else {
-            env.get_string((&o).into())?.into()
+            env.get_string(&o.into())?.into()
         }
-    } else {
-        return Ok(None);
     };
 
-    let j_records = env.get_field(o, "records", "Ljava/util/List;")?;
-    let JValueGen::Object(j_records) = j_records else {
-        return Ok(None);
+    let list = env.get_field(o, "records", "Ljava/util/List;")?;
+    let records = if let JValueGen::Object(list) = list {
+        parse_java_list(env, &list, |env, o| {
+            let score = env.get_field(o, "score", "I")?.i()?;
+            let time = env.get_field(o, "time", "J")?.j()?;
+            let timestamp = env.get_field(o, "timestamp", "J")?.j()?;
+            Ok(PlayerRecord {
+                score,
+                time,
+                timestamp,
+            })
+        })?
+    } else {
+        Vec::default()
     };
-    let mut records = Vec::new();
-    let size = env
-        .call_method(&j_records, "size", "()I", &[])
-        .unwrap()
-        .i()
-        .unwrap();
-    for i in 0..size {
-        let record = env
-            .call_method(
-                &j_records,
-                "get",
-                "(I)Ljava/lang/Object;",
-                &[jni::objects::JValueGen::Int(i)],
-            )
-            .unwrap();
-        let JValueGen::Object(o) = record else {
-            continue;
-        };
-        let score = env.get_field(&o, "score", "I")?.i()?;
-        let time = env.get_field(&o, "time", "J")?.j()?;
-        let timestamp = env.get_field(&o, "timestamp", "J")?.j()?;
-        records.push(PlayerRecord {
-            score,
-            time,
-            timestamp,
-        });
-    }
+
+    let (best_score, best_time, best_timestamp) = records
+        .iter()
+        .max_by_key(|x| x.score)
+        .map(|x| (x.score, x.time, x.timestamp))
+        .unwrap_or_default();
 
     Ok(Some(Player {
         id,
@@ -277,20 +299,210 @@ fn get_player_from_java<'local>(
     }))
 }
 
+fn new_player_record<'local>(
+    env: &mut JNIEnv<'local>,
+    id: i32,
+    record: PlayerRecord,
+) -> jni::errors::Result<JObject<'local>> {
+    env.new_object(
+        "com/smoother/TacticalGrid2048/entity/PlayerRecord",
+        "(IIJJ)V",
+        &[
+            JValueGen::Int(id),
+            JValueGen::Int(record.score),
+            JValueGen::Long(record.time),
+            JValueGen::Long(record.timestamp),
+        ],
+    )
+}
+
+fn new_player<'local>(
+    env: &mut JNIEnv<'local>,
+    player: Player,
+) -> jni::errors::Result<JObject<'local>> {
+    let array_list = env.new_object("java/util/ArrayList", "()V", &[])?;
+    let mut it = player.records.into_iter();
+    while let Some(x) = it.next() {
+        let record = new_player_record(env, player.id, x)?;
+        env.call_method(
+            &array_list,
+            "add",
+            "(Ljava/lang/Object;)Z",
+            &[JValueGen::Object(&record)],
+        )?;
+    }
+    let name = env.new_string(player.name)?;
+    env.new_object(
+        "com/smoother/TacticalGrid2048/entity/Player",
+        "(ILjava/lang/String;Ljava/util/List;)V",
+        &[
+            JValueGen::Int(player.id),
+            JValueGen::Object(&name),
+            JValueGen::Object(&array_list),
+        ],
+    )
+}
+
 pub fn get_current_player(
     env: &mut JNIEnv<'_>,
     service: &JObject<'_>,
 ) -> jni::errors::Result<Player> {
-    let player = env
+    let player: JObject<'_> = env
         .call_method(
             service,
             "getCurrentPlayer",
             "()Lcom/smoother/TacticalGrid2048/entity/Player;",
             &[],
-        )
-        .unwrap();
+        )?
+        .try_into()?;
+    get_player_from_java(env, &player).map(|x| x.unwrap_or_default())
+}
+
+pub fn get_players_best_except_self(
+    env: &mut JNIEnv<'_>,
+    service: &JObject<'_>,
+) -> jni::errors::Result<Vec<Player>> {
+    let player = env.call_method(
+        service,
+        "getPlayersBestExceptSelf",
+        "()Ljava/util/List;",
+        &[],
+    )?;
     let JValueGen::Object(o) = player else {
-        return Ok(Player::default());
+        return Ok(Vec::default());
     };
-    get_player_from_java(env, o).map(|x| x.unwrap_or_default())
+    let list = parse_java_list(env, &o, |env, o| get_player_from_java(env, o))?;
+    let mut result = Vec::new();
+    list.into_iter().for_each(|x| {
+        if let Some(x) = x {
+            result.push(x);
+        }
+    });
+    Ok(result)
+}
+
+pub fn get_players_except_self(
+    env: &mut JNIEnv<'_>,
+    service: &JObject<'_>,
+) -> jni::errors::Result<Vec<Player>> {
+    let player = env.call_method(service, "getPlayersExceptSelf", "()Ljava/util/List;", &[])?;
+    let JValueGen::Object(o) = player else {
+        return Ok(Vec::default());
+    };
+    let list = parse_java_list(env, &o, |env, o| get_player_from_java(env, o))?;
+    let mut result = Vec::new();
+    list.into_iter().for_each(|x| {
+        if let Some(x) = x {
+            result.push(x);
+        }
+    });
+    Ok(result)
+}
+
+pub fn save_current_player(
+    env: &mut JNIEnv<'_>,
+    service: &JObject<'_>,
+    player: Player,
+) -> jni::errors::Result<bool> {
+    let player = new_player(env, player)?;
+    env.call_method(
+        service,
+        "saveCurrentPlayer",
+        "(Lcom/smoother/TacticalGrid2048/entity/Player;)Z",
+        &[JValueGen::Object(&player)],
+    )?
+    .z()
+}
+
+pub fn verify_account(
+    env: &mut JNIEnv<'_>,
+    service: &JObject<'_>,
+    username: String,
+    password: String,
+) -> jni::errors::Result<Option<Player>> {
+    let username = env.new_string(username)?;
+    let password = env.new_string(password)?;
+    let result: JObject<'_> = env
+        .call_method(
+            service,
+            "verifyAccount",
+            "(Ljava/lang/String;Ljava/lang/String;)Lcom/smoother/TacticalGrid2048/entity/Player;",
+            &[JValueGen::Object(&username), JValueGen::Object(&password)],
+        )?
+        .try_into()?;
+    get_player_from_java(env, &result)
+}
+
+pub fn register_account(
+    env: &mut JNIEnv<'_>,
+    service: &JObject<'_>,
+    username: String,
+    password: String,
+) -> jni::errors::Result<Option<Player>> {
+    let username = env.new_string(username)?;
+    let password = env.new_string(password)?;
+    let result: JObject<'_> = env
+        .call_method(
+            service,
+            "registerAccount",
+            "(Ljava/lang/String;Ljava/lang/String;)Lcom/smoother/TacticalGrid2048/entity/Player;",
+            &[JValueGen::Object(&username), JValueGen::Object(&password)],
+        )?
+        .try_into()?;
+    get_player_from_java(env, &result)
+}
+
+pub fn find_player(
+    env: &mut JNIEnv<'_>,
+    service: &JObject<'_>,
+    player: Player,
+) -> jni::errors::Result<Vec<Player>> {
+    let player = new_player(env, player)?;
+    let player = env.call_method(
+        service,
+        "findPlayer",
+        "(Lcom/smoother/TacticalGrid2048/entity/Player;)Ljava/util/List;",
+        &[JValueGen::Object(&player)],
+    )?;
+    let JValueGen::Object(o) = player else {
+        return Ok(Vec::default());
+    };
+    let list = parse_java_list(env, &o, |env, o| get_player_from_java(env, o))?;
+    let mut result = Vec::new();
+    list.into_iter().for_each(|x| {
+        if let Some(x) = x {
+            result.push(x);
+        }
+    });
+    Ok(result)
+}
+
+pub fn update_player(
+    env: &mut JNIEnv<'_>,
+    service: &JObject<'_>,
+    player: Player,
+) -> jni::errors::Result<bool> {
+    let player = new_player(env, player)?;
+    env.call_method(
+        service,
+        "updatePlayer",
+        "(Lcom/smoother/TacticalGrid2048/entity/Player;)Z",
+        &[JValueGen::Object(&player)],
+    )?
+    .z()
+}
+
+pub fn remove_player(
+    env: &mut JNIEnv<'_>,
+    service: &JObject<'_>,
+    player: Player,
+) -> jni::errors::Result<bool> {
+    let player = new_player(env, player)?;
+    env.call_method(
+        service,
+        "removePlayer",
+        "(Lcom/smoother/TacticalGrid2048/entity/Player;)Z",
+        &[JValueGen::Object(&player)],
+    )?
+    .z()
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,91 @@
-use std::{sync::mpsc, thread};
+use std::{
+    sync::mpsc::{self, Receiver, Sender},
+    thread,
+};
 
 use app::{
     data::jni::{JniDataManager, Request, RequestBody, Response, ResponseBody},
     entry::leave,
 };
 use jni::{
-    JNIEnv,
+    JNIEnv, JavaVM,
     objects::{JClass, JValueGen},
 };
 
 mod app;
+
+fn start_thread(rx: Receiver<Request>, tx: Sender<Response>, vm: JavaVM) {
+    thread::spawn(move || {
+        let rx = rx;
+        let tx = tx;
+        let mut env = vm.attach_current_thread_as_daemon().unwrap();
+        let player_service = env
+            .find_class("com/smoother/TacticalGrid2048/GlobalVariants")
+            .unwrap();
+        let service = env
+            .get_static_field(
+                player_service,
+                "playerService",
+                "Lcom/smoother/TacticalGrid2048/service/PlayerService;",
+            )
+            .unwrap();
+        let JValueGen::Object(service) = service else {
+            panic!("Type of PlayerService is incorrect")
+        };
+
+        loop {
+            let req = rx.recv().unwrap();
+            let rsp: ResponseBody;
+            match req.0 {
+                RequestBody::GetCurrentPlayer => {
+                    let result = app::data::jni::get_current_player(&mut env, &service).unwrap();
+                    rsp = ResponseBody::GetCurrentPlayer(result);
+                }
+                RequestBody::VerifyAccount(username, password) => {
+                    let result =
+                        app::data::jni::verify_account(&mut env, &service, username, password)
+                            .unwrap();
+                    rsp = ResponseBody::VerifyAccount(result);
+                }
+                RequestBody::RegisterAccount(username, password) => {
+                    let result =
+                        app::data::jni::register_account(&mut env, &service, username, password)
+                            .unwrap();
+                    rsp = ResponseBody::RegisterAccount(result);
+                }
+                RequestBody::GetPlayersBestExceptSelf => {
+                    let result =
+                        app::data::jni::get_players_best_except_self(&mut env, &service).unwrap();
+                    rsp = ResponseBody::GetPlayersBestExceptSelf(result);
+                }
+                RequestBody::GetPlayersExceptSelf => {
+                    let result =
+                        app::data::jni::get_players_except_self(&mut env, &service).unwrap();
+                    rsp = ResponseBody::GetPlayersExceptSelf(result);
+                }
+                RequestBody::SaveCurrentPlayer(player) => {
+                    let result =
+                        app::data::jni::save_current_player(&mut env, &service, player).unwrap();
+                    rsp = ResponseBody::SaveCurrentPlayer(result)
+                }
+                RequestBody::FindPlayer(player) => {
+                    let result = app::data::jni::find_player(&mut env, &service, player).unwrap();
+                    rsp = ResponseBody::FindPlayer(result)
+                }
+                RequestBody::UpdatePlayer(player) => {
+                    let result = app::data::jni::update_player(&mut env, &service, player).unwrap();
+                    rsp = ResponseBody::UpdatePlayer(result)
+                }
+                RequestBody::RemovePlayer(player) => {
+                    let result = app::data::jni::remove_player(&mut env, &service, player).unwrap();
+                    rsp = ResponseBody::RemovePlayer(result)
+                }
+                RequestBody::Exit => break,
+            }
+            tx.send((rsp, req.1)).unwrap();
+        }
+    });
+}
 
 #[unsafe(no_mangle)]
 pub extern "system" fn Java_com_smoother_TacticalGrid2048_view_View_startAndJoin<'local>(
@@ -44,41 +120,16 @@ pub extern "system" fn Java_com_smoother_TacticalGrid2048_view_View_startAndJoin
         println!("Message: {s:?}");
     }));
 
-    thread::spawn(move || {
-        let rx = req_rx;
-        let tx = rsp_tx;
-        let mut env = vm.attach_current_thread().unwrap();
-        let player_service = env
-            .find_class("com/smoother/TacticalGrid2048/GlobalVariants")
-            .unwrap();
-        let service = env
-            .get_static_field(
-                player_service,
-                "playerService",
-                "Lcom/smoother/TacticalGrid2048/service/PlayerService;",
-            )
-            .unwrap();
-        let JValueGen::Object(service) = service else {
-            panic!("Type of PlayerService is incorrect")
-        };
+    start_thread(req_rx, rsp_tx, vm);
 
-        loop {
-            let req = rx.recv().unwrap();
-            let rsp: ResponseBody;
-            match req.0 {
-                RequestBody::GetCurrentPlayer => {
-                    let result = app::data::jni::get_current_player(&mut env, &service).unwrap();
-                    rsp = ResponseBody::GetCurrentPlayer(result);
-                }
-                _ => todo!(),
-            }
-            tx.send((rsp, req.1)).unwrap();
-        }
-    });
-
-    let data_manager = Box::new(JniDataManager::new(req_tx, rsp_rx, is_first_launch));
+    let data_manager = Box::new(JniDataManager::new(req_tx.clone(), rsp_rx, is_first_launch));
     if let Err(e) = app::entry::run_app(data_manager) {
         env.throw(("java/io/IOException", format!("{e:?}")))
+            .unwrap();
+        return;
+    }
+    if let Err(e) = req_tx.send((RequestBody::Exit, 0)) {
+        env.throw(("java/lang/IllegalStateException", format!("{e:?}")))
             .unwrap();
         return;
     }
